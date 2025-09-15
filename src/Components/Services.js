4045b24a8f9b92de1f9fbffafa28958572171bf9
Alex Goode <alexgoodestudio@gmail.com>
2025-09-15 19:54:23 -0400

    font stylings

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -2,7 +2,7 @@ import Typewriter from "./TypeWriter";
 
 function Services() {
   return (
-    <div className="bg-white vh-100 text-black px-5 py-5">
+    <div className="bg-white services text-black px-5 py-5">
       <h2 className="text-3xl font-bold mb-6 text-start ">What We Do.</h2>
           <section className="border br pb-3 pt-4 px-4 d-flex "><Typewriter/></section>
       <ul className="list-disc mt-5 text-start list-inside space-y-2">
