dd37790aea6c0a63ef87e46047113311b6c70180
Alex Goode <alexgoodestudio@gmail.com>
2025-10-22 20:52:12 -0400

    updated NAv BAnner

--- src/Components/NavBanner.js
+++ src/Components/NavBanner.js
@@ -1,8 +1,8 @@
 function NavBanner() {
   return (
     <div className="bg-sky-200 text-sky-800 text-center pt-3 pb-1">
-      <p className="text-base font-medium tracking-wide">
-       ✄┈ Made Right Studio — Web Design & Development — Columbia, South Carolina
+      <p className="text-md tracking-wide">
+        Web Design & Development — Columbia, South Carolina
       </p>
     </div>
   );
