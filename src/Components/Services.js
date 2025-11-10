c382322abb00483292f22457db6c5bf7981a82cd
Alex Goode <alexgoodestudio@gmail.com>
2025-11-10 15:50:24 -0500

    services section typewriter removed

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,8 +1,6 @@
 import React, { useRef } from 'react';
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
-import Typewriter from './TypeWriter';
-import { Link } from "react-router-dom";
 
 function Services() {
   const containerRef = useRef();
@@ -110,17 +108,11 @@ function Services() {
               </div>
             </div>
 
-            <div className="row mb-5">
-              <div className="col-12 col-lg-10">
-                <div className="">
-                  <Typewriter />
-                </div>
-              </div>
-            </div>
+
 
             <div className="row g-0">
               
-              <div className="col-12 col-lg-8">
+              <div className="col-12 col-lg-12">
                 <div className="border-top border-start border-slate-900">
                   {services.map((service, index) => (
                     <div 
@@ -153,29 +145,7 @@ function Services() {
                 </div>
               </div>
 
-              <div className="col-12 col-lg-4">
-                <div className="p-4 bg-slate-900 text-white h-100 d-flex flex-column justify-content-center">
-                  <div className="text-xs text-slate-400 mb-3">
-                    Our Approach
-                  </div>
-                  <h3 className="text-2xl font-medium mb-3">
-                    Modular by Design
-                  </h3>
-                  <p className="text-sm text-slate-300 mb-4">
-                    Every project begins with understanding your goals. We build 
-                    component-based systems that evolve with your business.
-                  </p>
-                 
-                  <div className="mt-auto pt-4">
-                    <Link 
-                      to="/our-process" 
-                      className="text-white text-sm font-medium"
-                    >
-                      View Our Process →
-                    </Link>
-                  </div>
-                </div>
-              </div>
+
               
             </div>
 
