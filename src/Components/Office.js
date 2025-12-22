e35be479249d2f9de8aa00cb45fea1f5683148ed
Alex Goode <alexgoodestudio@gmail.com>
2025-12-21 21:23:07 -0500

    typo fix

--- src/Components/Office.js
+++ src/Components/Office.js
@@ -18,19 +18,20 @@ function Office() {
         <p
           className="text-gray-700 gs mt-4 text-xl text-start px-lg-3 py-lg-3"
           style={{
-            letterSpacing: '-0.01em',
-            lineHeight: '1.6'
+            letterSpacing: "-0.01em",
+            lineHeight: "1.6",
           }}
         >
           Made Right Studio is just me, working from home in my free time,
-          developing and designing websites. I live in the
-          Rosewood-Shandon neighborhood of Columbia, South Carolina, and work
-          full-time stocking shelves at Whole Foods Market. After hours, I'm
-          building and designing websites combining what I learned from Promineo Tech's Front End
+          developing and designing websites. I live in the Rosewood-Shandon
+          neighborhood of Columbia, South Carolina, and work full-time stocking
+          shelves at Whole Foods Market. After hours, I'm building and designing
+          websites combining what I learned from Promineo Tech's Front End
           Development Program and Thinkful's Full-Stack Engineering Immersion
-          that I completed between 2021-2023, with four years of graphic design
-          experience. The goal is to create design-first websites that have the technical execution of top-performing websites.
-          help local businesses connect with their customers.
+          Program that I completed between 2021-2023. With four years prior of
+          graphic design experience, my goal is to create design-first websites
+          that have the technical execution of top-performing websites, helping
+          local businesses connect with their customers.
         </p>
       </div>
     </section>
